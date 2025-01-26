5d8025bc13675d9ca9447a2233218ea1db8bc714
Lucas Vitareli <lucas.vitareli@hotmail.com>
2025-01-26 15:38:03 +0000

    done sections, hero, info, trailers, more details

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+//const uglify = require('gulp-uglify');
+
+
+function styles() {
+    return gulp.src('./src/styles/*.scss') 
+        .pipe(sass({ outputStyle: 'compressed' })) 
+        .pipe(concat('main.css')) 
+        .pipe(gulp.dest('./dist/css')); 
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+/*function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}*/
+
+function watch() {
+    gulp.watch('./src/styles/**/*.scss', styles);
+    gulp.watch('./src/images/**/*', images);
+
+
+}
+
+exports.build = gulp.parallel(styles, images, );
+exports.dev = gulp.series(exports.build, watch);
+
+
